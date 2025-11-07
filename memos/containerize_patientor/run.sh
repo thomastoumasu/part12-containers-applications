ebaf0c66be65872ad94d519dbb8f027b81a970b8
thomastoumasu <thomastoumasu@protonmail.com>
2025-11-07 21:50:22 +0100

    removed Patientor code

--- memos/containerize_patientor/run.sh
+++ memos/containerize_patientor/run.sh
@@ -1,4 +1,5 @@
-# use this script to build the image specified in Docerfile and run the container
+# just copy Patientor_fullstack in this folder. Then
+# use this script to build the image specified in Dockerfile and run the container
 # or better, use the docker-compose.yml 
 # # look at the folder structure inside the container (overwrite CMD in dockerfile):
 # docker build -t my-patientor.
